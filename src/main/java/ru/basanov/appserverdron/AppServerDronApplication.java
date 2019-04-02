0ba39ba4a20b17bff07aa0c088b04fbad9e9ff33
Mingiyan1987 <basanov.mt87@gmail.com>
2019-04-02 22:26:12 +0300

    Создал модель данных.

---
+++ src/main/java/ru/basanov/appserverdron/AppServerDronApplication.java
@@ -0,0 +1,13 @@
+package ru.basanov.appserverdron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServerDronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppServerDronApplication.class, args);
+    }
+
+}
